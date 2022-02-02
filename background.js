dfb2f93b1caf660c057a8b7a876fa279f68b9e04
ganesh <ganeshece1317@gmail.com>
2022-02-02 01:26:37 -0500

    Modified background.js

--- background.js
+++ background.js
@@ -1,1 +1,2 @@
+//Reference: https://stackoverflow.com/questions/66055882/chrome-extensions-use-the-background-service-worker-key-instead-manifest-vers
 console.log("Message from extension: \"sample domain blocker\" - Hello World!");
